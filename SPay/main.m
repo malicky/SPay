626b5aae5fea1f4e303d81f2322b2260abdb9c45
malick youla <myoula@videotron.ca>
2014-10-13 12:36:14 +0300

    Initial Commit

---
+++ SPay/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SPay
+//
+//  Created by Malick Youla on 2014-10-13.
+//  Copyright (c) 2014 Malick Youla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SPayTests/SPayTests.m
@@ -0,0 +1,34 @@
+//
+//  SPayTests.m
+//  SPayTests
+//
+//  Created by Malick Youla on 2014-10-13.
+//  Copyright (c) 2014 Malick Youla. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SPayTests : XCTestCase
+
+@end
+
+@implementation SPayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
